0596874bc2b0d109d7ceaf8aa4506959940c6ec5
KIRSTEN-23 <231296@virtualwindow.co.za>
2023-05-28 20:44:44 +0200

    Exercise 4

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let john = 75
+let tim = 60
+let alfie = 80
+let gwen = 95
+let studentCount = 4
+
+john = john + 7
+tim = tim + 7
+alfie = alfie + 7
+gwen = gwen + 7
+
+let valueOne = john + tim + alfie + gwen
+let valueTwo = valueOne / studentCount
+
+let answer = "The students updated average is: " + valueTwo + "/100"
+
+console.log(answer)
